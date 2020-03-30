5f54864feb1c0672c4950bbf51764ec95834ff41
Rajeev <raghu2raj@gmail.com>
2020-03-30 21:37:38 +0530

    mod to run other programs

--- gopointer.go
+++ gopointer.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func createPointer(f *float64) {
 	fmt.Print(&f)
 }
 
-func main() {
-	var myInt int
-	fmt.Println(reflect.TypeOf(&myInt))
-	var1 := 56.36
-	createPointer(&var1)
-}
+// func main() {
+// 	var myInt int
+// 	fmt.Println(reflect.TypeOf(&myInt))
+// 	var1 := 56.36
+// 	createPointer(&var1)
+// }
--- pointerExample.go
+++ pointerExample.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 )
 
+//pointers inside functions
+func returnPointer() *float64 {
+	flotingNumber := 98.5
+	return &flotingNumber
+}
+
+func printValAtPointer(poi *float64) {
+	fmt.Println(*poi)
+}
+
 // func main() {
-// 	variable := 5.6
-// 	fmt.Println("VarValue:", variable)
-// 	fmt.Println("VarAddress:", &variable)
+// 	poi := returnPointer()
+// 	fmt.Println(poi)
+// 	// prints 0xc000012090
+
+// 	printValAtPointer(poi)
+// 	//prints 98.5
+
 // }
