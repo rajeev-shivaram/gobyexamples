c5815213e139dff67b2eb1a49b1541624ee330fa
Rajeev <raghu2raj@gmail.com>
2020-03-31 23:17:33 +0530

    add go dir format and comment typing

---
+++ src/greeting/greeting.go
@@ -0,0 +1,14 @@
+// Package greeting is used to say hello
+package greeting
+
+import "fmt"
+
+// Hello says hello
+func Hello() {
+	fmt.Println("Hello")
+}
+
+// Hi says hi
+func Hi() {
+	fmt.Println("Hi!")
+}
